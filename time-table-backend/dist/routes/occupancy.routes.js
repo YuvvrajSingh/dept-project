6332ec2b24bbb2bd13feda475462390408672ed7
Yuvraj Singh <130268945+YuvvrajSingh@users.noreply.github.com>
2026-04-12 23:31:37 +0530

    refactor: remove unused services and utility files
    - Deleted scheduler.service.js, subject.service.js, teacher.service.js, timetable.service.js, user.service.js, AppError.js, authCookie.js, jwtExpiresMs.js, timetableConstants.js, and timetableMatrix.js.
    - These files were no longer needed in the codebase, streamlining the project structure and reducing complexity.

--- time-table-backend/dist/routes/occupancy.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const occupancy_controller_1 = require("../controllers/occupancy.controller");
-const router = (0, express_1.Router)();
-router.get("/", occupancy_controller_1.occupancyController.getMatrix);
-exports.default = router;
--- time-table-backend/dist/utils/authCookie.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sessionCookieMaxAgeMs = sessionCookieMaxAgeMs;
-const auth_1 = require("../constants/auth");
-const jwtExpiresMs_1 = require("./jwtExpiresMs");
-function sessionCookieMaxAgeMs() {
-    const expiresIn = process.env.JWT_EXPIRES_IN?.trim() || auth_1.DEFAULT_JWT_EXPIRES_IN;
-    return (0, jwtExpiresMs_1.jwtExpiresInToMs)(expiresIn);
-}
